5e8c9b9cc379d41dd506310fc720031fb1c03874
Justyna Chrostek <justyna.maria.chrostek@gmail.com>
2020-10-02 16:26:23 +0200

    Change of format and style

--- src/components/layout/ValueWithLabel.js
+++ src/components/layout/ValueWithLabel.js
@@ -1,12 +1,12 @@
-import React, { Fragment} from "react";
+import React, { Fragment } from "react";
 
-function ValueWithLabel({val, onChange, label, colorClassName}) {
-   return (
-      <Fragment>
-        <label className={"btn-label " + colorClassName}>{label}</label>
-        <input value={val} onChange={onChange} />
-      </Fragment>
-    );
+function ValueWithLabel({ val, onChange, label, colorClassName }) {
+  return (
+    <Fragment>
+      <label className={"btn-label " + colorClassName}>{label}</label>
+      <input className="input" value={val} onChange={onChange} />
+    </Fragment>
+  );
 }
 
 export default ValueWithLabel;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,24 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-   return (
-      <Fragment>
-         <h1 className="card-header main-navbar">Converter</h1>
-         <div className="card-body">
-            <p className="card-text">What you need to convert?</p>
-            <Link className="btn btn-block btn-primary-color" to="/temp">Temperature</Link>
-            <Link className="btn btn-block btn-secondary-color" to="/weight">Weight</Link>
-            <Link className="btn btn-block btn-teritiary-color" to="/currency">Currency</Link>
-         </div>
-      </Fragment>
-   )
+  return (
+    <Fragment>
+      <h1 className="card-header main-navbar">Converter</h1>
+      <div className="card-body">
+        <p className="card-text">What you need to convert?</p>
+        <Link className="btn btn-block btn-primary-color" to="/temp">
+          Temperature
+        </Link>
+        <Link className="btn btn-block btn-secondary-color" to="/weight">
+          Weight
+        </Link>
+        <Link className="btn btn-block btn-teritiary-color" to="/currency">
+          Currency
+        </Link>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Home;
